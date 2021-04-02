e2ce88bc1f92a51180a3ea0ed78482664eefa73b
gurpreet <gurpreetkaur.devgun@yahoo.com>
2021-04-02 10:57:29 -0700

    no message

--- src/components/Sidebar/Drawer.jsx
+++ src/components/Sidebar/Drawer.jsx
@@ -21,22 +21,24 @@ class Drawer extends Component {
   render() {
     const {islight} = this.state;
     return (
-      <aside className="col-12 col-md-3 col-lg-2 px-0 drawer">
-        <div className="drawer-header">
-          <ul>
-            <li>
-              <a href="javascript:void(0)" onClick={this.props.themeToggler} className="nav-link theme-mode">
-                <div className="toggle-icon-changer">
-                  <i className={`theme-icon theme-icon-left fa fa-sun-o ${this.props.theme === "light" ? "theme-active":null }`}></i>
-                  <div className="theme-separator">/</div>
-                  <i className={`theme-icon theme-icon-right fa fa-moon-o ${this.props.theme === "light" ? null : "theme-active"}`}></i>
-                </div>
-                <span onClick={()=>this.setState({islight:true})}>Theme Mode</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </aside>
+      <>
+      {/* // <aside className="col-12 col-md-3 col-lg-2 px-0 drawer">
+      //   <div className="drawer-header">
+      //     <ul>
+      //       <li>
+      //         <a href="javascript:void(0)" onClick={this.props.themeToggler} className="nav-link theme-mode">
+      //           <div className="toggle-icon-changer">
+      //             <i className={`theme-icon theme-icon-left fa fa-sun-o ${this.props.theme === "light" ? "theme-active":null }`}></i>
+      //             <div className="theme-separator">/</div>
+      //             <i className={`theme-icon theme-icon-right fa fa-moon-o ${this.props.theme === "light" ? null : "theme-active"}`}></i>
+      //           </div>
+      //           <span onClick={()=>this.setState({islight:true})}>Theme Mode</span>
+      //         </a>
+      //       </li>
+      //     </ul>
+      //   </div>
+      // </aside> */}
+      </>
     )
   }
 }

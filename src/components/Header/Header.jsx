15c83e0ec0fd495799e030b78ead4777e7b999a2
Abhishek <sharmaabhishek098@gmail.com>
2021-04-02 23:27:03 +0530

    no message

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import Header from "./Header.scss"
+
+class HeaderMian extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render() {
+    return <div>hi i am header.....</div>
+  }
+}
+
+export default HeaderMian

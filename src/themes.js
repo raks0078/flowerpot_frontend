85dd88c16a61b8264007b4ccf08346ca36e8cd08
gurpreet <gurpreetkaur.devgun@yahoo.com>
2021-04-03 12:16:13 -0700

    no message

--- src/themes.js
+++ src/themes.js
@@ -3,11 +3,21 @@ import { createGlobalStyle } from "styled-components"
 export const lightTheme = {
   body: "#9DC88D",
   fontColor: "#4D774E",
+  navshadow: "0 0 5px rgba(22, 74, 65, .5)",
+  commoncolor: "#164A41",
+  oppositcolor: "#9DC88D",
+  buttonhover: "#1c584e",
+  drawershadow: "0 7px 5px rgba(22, 74, 65, .5)",
 }
 
 export const darkTheme = {
-  body: "#020416",
-  fontColor: "#cacedb",
+  body: "#164A41",
+  fontColor: "#4D774E",
+  navshadow: "0 0 5px rgba(77, 119, 78, .5)",
+  commoncolor: "#9DC88D",
+  oppositcolor: "#164A41",
+  buttonhover: "#8cbb7a",
+  drawershadow: "0 7px 5px rgba(77, 119, 78, .5)",
 }
 
 export const GlobalStyle = createGlobalStyle`
@@ -16,8 +26,46 @@ export const GlobalStyle = createGlobalStyle`
     body {
             background-color: ${(props) => props.theme.body};
         }
-
-
-
+        nav{
+          box-shadow: ${(props) => props.theme.navshadow};
+        }
+        nav .container-fluid .nav-left button svg path {
+          fill: ${(props) => props.theme.commoncolor};
+      }
+      nav .container-fluid .nav-left a {
+        color: ${(props) => props.theme.commoncolor};
+    }
+    nav .container-fluid .nav-right button{
+      background-color: ${(props) => props.theme.commoncolor};
+      color: ${(props) => props.theme.oppositcolor};
+      border-color: ${(props) => props.theme.commoncolor};
+    }
+    nav .container-fluid .nav-right button:hover{
+      background-color: ${(props) => props.theme.buttonhover};
+      border-color: ${(props) => props.theme.buttonhover};
+    }
+    .drawer{
+      box-shadow: ${(props) => props.theme.drawershadow};
+    }
+    .drawer ul li a span svg path {
+      fill: ${(props) => props.theme.commoncolor};
+  }
+  .drawer ul .collapse, .drawer ul .collapsing{
+    background-color: ${(props) => props.theme.commoncolor};
+  }
+  .drawer ul li a.active {
+    border-left-color: ${(props) => props.theme.commoncolor};
+}
+.drawer ul .theme-mode a .toggle-icon-changer .theme-icon-left{
+  color: ${(props) => props.theme.commoncolor};
+}
+.drawer .aside-bottom .social a svg path {
+  fill: ${(props) => props.theme.commoncolor};
+}
+.farms-content .tool-wrap, 
+.pools-content .tool-wrap, 
+.ifo-content .tool-wrap{
+  background-color: ${(props) => props.theme.commoncolor};
+}
 
 `

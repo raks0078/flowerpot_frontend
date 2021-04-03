467763e4c018eafa60fad57eec5309c2c1a0dcb4
Abhishek <sharmaabhishek098@gmail.com>
2021-04-03 11:10:38 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import { lightTheme, darkTheme, GlobalStyle } from "./themes"
 import Drawer from "../src/components/Sidebar/Drawer"
 import Header from "../src/components/Header/Header"
 import Launchpad from "./pages/Launchpad"
+import Pools from "./pages/Pools"
+import Ifo from "./pages/Ifo"
+import Liquidity from "./pages/Liquidity"
+import Nftc from "./pages/Nftc"
+import Farms from "./pages/Farms"
+import TierList from "./pages/TierList"
+import Exchange from "./pages/Exchange"
+
+
 const StyledDiv = styled.div`
   color: ${(props) => props.theme.fontColor};
   background-color: ${(props) => props.theme.sidebarBgColor};
@@ -25,9 +34,16 @@ const App = () => {
         <div className="content-right">
           <Switch>
             <Route exact path="/launchpad" component={() => <Launchpad theme={theme} />} />
+            <Route exact path="/pools" component={() => <Pools theme={theme} />} />
+            <Route exact path="/ifo" component={() => <Ifo theme={theme} />} />
+            <Route exact path="/liqudity" component={() => <Liquidity theme={theme} />} />
+            <Route exact path="/nftc" component={() => <Nftc theme={theme} />} />
+            <Route exact path="/farms" component={() => <Farms theme={theme} />} />
+            <Route exact path="/tierList" component={() => <TierList theme={theme} />} />
+            <Route exact path="/exchange" component={() => <Exchange theme={theme} />} />
+      
           </Switch>
         </div>
-        {/* <Footer /> */}
       </StyledDiv>
     </ThemeProvider>
   )
---
+++ src/pages/LaunchReq.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./LaunchReq.scss"
+class LaunchReq extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default LaunchReq
